3f42c6f58a34cffa097c1ab86752d4421ff801b2
Harshitha-200519 <harshithareddytineti@gmail.com>
2026-06-11 12:24:26 +0530

    Added Flask app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "AI FAQ Chatbot Running Successfully!"
+
+if _name_ == '_main_':
+    app.run(debug=True)
