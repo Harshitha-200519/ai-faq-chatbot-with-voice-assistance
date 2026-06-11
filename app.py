88c3e866e37e97b0c29f279e944757c53d75bec4
Harshitha-200519 <harshithareddytineti@gmail.com>
2026-06-11 14:21:40 +0530

    Basic FAQ chatbot completed

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+faqs = {
+    "what is ai": "AI means Artificial Intelligence.",
+    "what is python": "Python is a programming language.",
+    "what is flask": "Flask is a Python web framework."
+}
+
 @app.route('/')
 def home():
-    return "AI FAQ Chatbot Running Successfully!"
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_msg = request.json['message'].lower()
+
+    response = "Sorry, I don't know that."
+
+    for key in faqs:
+        if key in user_msg:
+            response = faqs[key]
+
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
